d24e4593bd34028180db5854f400ff8a146cb9e3
Harrison-Chacon-LN <harrison.chacon@bglobalsolutions.com>
2021-06-24 17:19:27 -0300

    buttonCustom

---
+++ components/button.js
@@ -0,0 +1,17 @@
+import { Button, CircularProgress } from "@material-ui/core";
+
+export default function ButtonCustom({loading, text, icon, disabled}) {
+
+  return (
+    <Button
+      startIcon={icon ? icon : null}
+      disabled={loading || disabled}
+      variant="outline"
+    >
+      {text}
+      {loading && (
+        <CircularProgress size={24}/>
+      )}
+    </Button>
+  );
+}
--- pages/theme.js
+++ pages/theme.js
@@ -1,16 +1,17 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
+import { esES } from '@material-ui/core/locale';
 
 const colorDegradado = "linear-gradient(0.25turn, #111121, #C0FEFC)";
-const primaryColor = "#556cd6";
+const primaryColor = "#111121";
 const secundaryColor = "#cccc";
 const badgeColor = "linear-gradient(0.25turn, #f10707f7, #0606068c)";
 const iconButtonColor = "white";
 
-const containerHeight = "100vh"
-const containerMaxWidth = "90vw"
-const heightContainerNavbar = "4rem"
-const heightContainerFooter = "6rem"
+const containerHeight = "100vh";
+const containerMaxWidth = "90vw";
+const heightContainerNavbar = "4rem";
+const heightContainerFooter = "6rem";
 
 // Create a theme instance.
 const theme = createMuiTheme({
@@ -45,10 +46,32 @@ const theme = createMuiTheme({
         color: iconButtonColor,
       },
     },
+    MuiButton: {
+      root: {
+        borderRadius: "3px",
+        margin: "1rem",
+        textTransform: "uppercase",
+        padding: "0.5rem 1.5rem",
+        color:iconButtonColor,
+        position: "relative",
+        background: colorDegradado,
+        '&$disabled': {
+          background: secundaryColor,
+        },
+      },
+    },
+    MuiCircularProgress: {
+      root: {
+        position: "absolute",
+      },
+      colorPrimary: {
+        color: primaryColor,
+      },
+    },
   },
   navBarColor: colorDegradado,
   heightContainerNavbar: heightContainerNavbar,
-  heightContainerFooter: heightContainerFooter
-});
+  heightContainerFooter: heightContainerFooter,
+}, esES);
 
 export default theme;
