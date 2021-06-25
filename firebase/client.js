db4088a8c0bbdaf926c92fcd2dd6b77b79ba72b0
Harrison-Chacon-LN <harrison.chacon@bglobalsolutions.com>
2021-06-25 17:13:59 -0300

    firebase

---
+++ firebase/client.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyABCIN4BXlxqQHp257NQBuxeF_YzNmzteI",
+    authDomain: "first-d42d8.firebaseapp.com",
+    projectId: "first-d42d8",
+    storageBucket: "first-d42d8.appspot.com",
+    messagingSenderId: "748648274994",
+    appId: "1:748648274994:web:d0e586715caf07b130f02b",
+    measurementId: "G-MX59GGQH82"
+  };
